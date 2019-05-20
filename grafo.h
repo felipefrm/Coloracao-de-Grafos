ba1602cdbf7b867c9c880eaf71d4fb34fb949a11
ThalesMrad <39007379+ThalesMrad@users.noreply.github.com>
2019-05-20 19:37:26 -0300

    diminuiiu

--- grafo.h
+++ grafo.h
@@ -1,21 +1,13 @@
 #ifndef GRAFO
 #define GRAFO
 
-// struct grafo {
-//     int numVertices;
-//     int grau_max;
-//     int** arestas;
-//     int* grau;
-//     int cor;
-// }; typedef struct grafo Grafo;
-
 #define TRUE 1
 #define FALSE 0
 
 typedef struct grafo {
-	int vertice;
-	int aresta;
+	int numVertices;
 	int** MatAdj;        // Matriz de adjacencia
+	int* grau;
 } Grafo;
 
 
@@ -23,8 +15,8 @@ Grafo* inicializaGrafo(int numVertices);
 void liberaGrafo(Grafo* gr);
 int insereAresta(Grafo* gr, int i, int j);
 int removeAresta(Grafo* gr, int i, int j);
-void imprimeGrafo(Grafo* gr, int numVertices, FILE* arq);
-
+void imprimeGrafo(Grafo* gr, FILE* arq);
+void calculaGrau(Grafo *gr);
 
 
 #endif
