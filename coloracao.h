6f1ef97c830893f827eb7afcfe8b399db986ae41
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-05-21 13:10:02 -0300

    Update coloracao.h

--- coloracao.h
+++ coloracao.h
@@ -2,15 +2,18 @@
 #define COLORACAO
 #include "grafo.h"
 
-typedef struct indicevetor {
-  int vertice;
-  int grau;
-} indiceVetor;
+typedef struct vertice {
+	int id;
+	int grau;
+  int cor;
+  int corDefinitiva
+} Vertice;
 
 int verificaCor(int **MatAdj, int *cor, int corAtual, int v, int numVertices);
 int coloracaoVertice(int **MatAdj, int *cor, int k, int v, int numVertices);
 int Backtracking(int **MatAdj, int k, int src, int numVertices);
-void insertionSort(indiceVetor* ivetor, int numVertices);
-void inicializaVetorIndice(indiceVetor* ivetor, Grafo* gr);
+void insertionSort(Vertice* V, int numVertices);
+Vertice* inicializaVetorIndice(Grafo* gr);
 int heuristica1(Grafo* gr);
+
 #endif
